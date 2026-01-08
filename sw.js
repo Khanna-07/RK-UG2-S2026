1af88a8a86ff478fd9972532bf6c5647d6726003
RahulKhanna <rahul.p24@iiits.in>
2026-01-08 23:06:56 +0530

    Update service worker cache and asset paths

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-const CACHE = "ug2-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./attendance.js",
-  "./manifest.json"
+const CACHE_NAME = "ug2-attendance-v1";
+
+const FILES = [
+  "/RK-UG2-S2026/",
+  "/RK-UG2-S2026/index.html",
+  "/RK-UG2-S2026/style.css",
+  "/RK-UG2-S2026/script.js",
+  "/RK-UG2-S2026/attendance.js",
+  "/RK-UG2-S2026/manifest.json",
+  "/RK-UG2-S2026/icon-192.png",
+  "/RK-UG2-S2026/icon-512.png"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
